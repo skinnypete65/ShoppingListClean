d826fff946d76b81e4e4d527a42ad23d99d59f32
skinnypete65 <skinnypetetskipe@gmail.com>
2021-11-27 13:15:14 +0300

    Set up presentation layer

--- app/src/main/java/com/justnik/shoppinglistclear/domain/ShopItem.kt
+++ app/src/main/java/com/justnik/shoppinglistclear/domain/ShopItem.kt
@@ -2,9 +2,9 @@ package com.justnik.shoppinglistclear.domain
 
 data class ShopItem(
     var name: String,
-    var count: String,
+    var count: Int,
     var enabled: Boolean,
-    var id: Int = UNDEFINED_ID,
+    var id: Int = UNDEFINED_ID
 ){
     companion object{
         const val UNDEFINED_ID = -1
--- app/src/main/java/com/justnik/shoppinglistclear/domain/ShopListRepository.kt
+++ app/src/main/java/com/justnik/shoppinglistclear/domain/ShopListRepository.kt
@@ -1,9 +1,11 @@
 package com.justnik.shoppinglistclear.domain
 
+import androidx.lifecycle.LiveData
+
 interface ShopListRepository {
     fun addShopItem(shopItem: ShopItem)
     fun deleteShopItem(shopItem: ShopItem)
     fun editShopItem(shopItem: ShopItem)
     fun getShopItem(id: Int): ShopItem
-    fun getShopList(): List<ShopItem>
+    fun getShopList(): LiveData<List<ShopItem>>
 }
